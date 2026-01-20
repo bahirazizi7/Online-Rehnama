28e57182286fe787d554289220e3c268e3d4e20e
Bahir Azizi <bahirazizi7@gmail.com>
2026-01-20 18:43:31 +0500

    MongotDB shutdown

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connectDB = require('./src/config/db')
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+const mongoose = require("mongoose");
 
-connectDB()
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+const shutdown = async () => {
+  console.log("🛑 Shutting down server...");
+  await mongoose.connection.close();
+  console.log("✅ MongoDB connection closed");
+  process.exit(0);
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);

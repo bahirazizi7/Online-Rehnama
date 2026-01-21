9cf6de4a52b04552a962e762bb59b107a4b45737
Bahir Azizi <bahirazizi7@gmail.com>
2026-01-21 23:11:57 +0500

    Error handling, API Response Standardization

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const errorHandler = require("./middlewares/error.middleware");
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(errorHandler)
 
 
 module.exports = app
